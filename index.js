44131747be6a1b1b3f9f3349814b8ff044ff2c38
Abdulaziz6665 <mr.yunusobod@gmail.com>
2022-02-01 16:20:48 +0500

    test

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ app.get('/webhook', (req, res) => {
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
+  console.log('body')
+  console.log(body, 'body')
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
